6071cbb545254c807a66233e57db304bb5f2453d
soumildatta <soumildatta@gmail.com>
2024-09-17 10:37:46 -0600

    v1 is complete

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 from pdfquery import PDFQuery
+import os
 
-filename = str(input("Enter the filename: "))
-filename = '2201.08619.pdf'
+def _extract_pdf_title(pdf_path):
+    pdf = PDFQuery(pdf_path)
+    pdf.load()
 
-pdf = PDFQuery(filename)
-pdf.load()
+    text_elements = pdf.pq('LTTextLineHorizontal')  
+    text = [t.text for t in text_elements]
+    index = text.index('')
+    title = ''.join(text[:index]).strip()
 
-text_elements = pdf.pq('LTTextLineHorizontal')
+    if title == '':
+        title = pdf_path.split('/')[-1].replace('.pdf', '')
+        return title, False
 
-text = [t.text for t in text_elements]
+    return title, True
+    
+# print(_extract_pdf_title('.pdf'))
 
-index = text.index('')
+def rename_pdf_file(pdf_path):
+    title, changed = _extract_pdf_title(pdf_path)
+    new_file_name = f'{title}.pdf'
 
-title = ''.join(text[:index])
+    if changed:
+        path = os.path.join(os.path.dirname(pdf_path), new_file_name)
+        os.rename(pdf_path, path)
+        print(f"File {pdf_path} renamed to {new_file_name}")
+    else:
+        print(f"No title parsed for {pdf_path}. Skipping...")
 
-print(title)
+if __name__ == '__main__':
+    scan_dir = str(input("Enter the directory path to scan: "))
+
+    # list all pdf files in scan_dir
+    pdf_files = [f for f in os.listdir(scan_dir) if f.endswith('.pdf')]
+
+    print(f"Found {len(pdf_files)} pdf files in {scan_dir}")
+    print("Starting to rename files...")
+
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(scan_dir, pdf_file)
+        rename_pdf_file(pdf_path)
